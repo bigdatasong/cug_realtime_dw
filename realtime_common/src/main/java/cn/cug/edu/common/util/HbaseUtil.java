e7cebfd1941f00a9116ed73e78fff6e3233a5457
songsong <songsjps@163.com>
2024-08-06 22:59:06 +0800

    还是第二天，完成habseutil工具中的创建表之前的方法

---
+++ realtime_common/src/main/java/cn/cug/edu/common/util/HbaseUtil.java
@@ -0,0 +1,188 @@
+package cn.cug.edu.common.util;
+
+import javafx.scene.AmbientLight;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.hadoop.hbase.NamespaceDescriptor;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import javax.swing.event.TableColumnModelEvent;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * author song
+ * date 2024-08-05 21:31
+ * Desc 实现对habse 的库和表的一系列操作
+ *
+ *      首先需要一个连接对象， 即connection ,habase的connection对象是线程安全的 一个app中只创建一次，多线程进行共享
+ *      其次 对表的操作，比如对表的crud 创建表等操作需要一个admin对象
+ *          对表数据的crud，插入数据等 就需要一个table对象 ，admin对象和table对象都是通过connection连接对象来获取得到，
+ *          但是要注意创建一个表的话 就是需要通过admin对象的api createtable 来创建
+ *          ，并且table和admin都是线程不安全的
+ */
+public class HbaseUtil {
+
+    // 所以我们需要一个connection对象 在类中要想保持对象只有一个 就必须是单例，可以通过静态代码块加静态属性的方式来获取
+    public static Connection hbaseConnection = null;
+
+    static {
+
+        //habse的conncection对象可以通过connectionfactory来创建得到
+        //创建数据库的连接对象  对于这里的异常的处理 ，之所以捕获是因为 已经时静态代码块了 ，所以如果直接又是抛出就会到jvm，所以jvm就会崩溃
+        try {
+            // 在这里我们使用的时空参的方法来创建 ，是因为底层会读取配置文件的zk ，从而不需要自己手动的创建hadoop的配置对象以及往配置对象中添加配置项
+            //所以只需要在类路径下创建一个hbase-size.xml文件即可，里面放zk的地址
+            //因为客户端连habse集群的原理就是，只需要知道hbase的元数据存放在哪里即可，而hbase的元数据是存放在zk中的
+            //所以只需要在配置文件中放入zk的地址即可。
+            hbaseConnection = ConnectionFactory.createConnection();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    //定义一个方法实现对连接的关闭
+    public static void colseConnection(){
+        // 如果连接不为null才需要关闭
+        if (hbaseConnection != null){
+            try {
+                hbaseConnection.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    // 我们首先来定义一个创建库的方法，在hbase中库表示namespace的意思，有default 即默认库的意思，便于区分 我们一般在可以创建一个新库来使用
+    // 创建库 需要用到admin来创建 ，而admin通过连接对象来创建
+    // 因为table对象也需要通过admin来 不妨我们先定义一个方法 来获取admin对象，因为admin对象在多个地方都会用到所以抽取出一个方法
+
+    //先创建一个能够得到admin对象的方法
+    public static Admin getAdmin() throws IOException {
+        Admin admin = hbaseConnection.getAdmin();
+
+        return admin;
+    }
+
+    // 然后开始创建一个能够创建库的方法 参数就是库名以及admin对象传入
+    //但是在创建库之前 同样也要知道这个库是否存在 所以需要在创建之前再编写一个判断库是否存在的方法
+
+    // 所以先编写一个库是否存在的方法 参数就是admin 以及 库名
+    public static Boolean checkNameSpaceExist(Admin admin,String nameSpace){
+        //首先需要对参数进行判断是否合法
+        //通过isanyblank来判断，只要可变参数有一个非法的就返回true 注意是或的关系
+        if (StringUtils.isAnyBlank(nameSpace)) {
+            //返回true说明他是非法的参数，那么就可以抛异常 或者说通过日志来打印
+            throw  new RuntimeException("库名非法！！！");
+        }else {
+            // 通过admin的getNamespaceDescriptor 来判断如果说通过库名 没有得到库描述的话 就会抛出异常
+            //那么我们可以通过是否有异常来返回true或者false；
+            try {
+                admin.getNamespaceDescriptor(nameSpace);
+                return true;
+            } catch (IOException e) {
+                return false;
+            }
+        }
+    }
+
+    //这个才是真正创建库的方法
+    public static void createNameSpace(Admin admin,String nameSpace) throws IOException {
+
+        // 首先需要判断库是否存在如果存在就不需要创建了 直接抛异常
+        Boolean aBoolean = checkNameSpaceExist(admin, nameSpace);
+        if (aBoolean) {
+            //说明库存在
+            throw new RuntimeException("库已经存在！！！");
+        }
+        // 不用else也可以，因为如果上面走不了 就直接抛异常 程序就直接停止了 所以肯定不会走下面的代码
+        // 其次判断库名是否合法
+        if (StringUtils.isAnyBlank(nameSpace)) {
+            // 直接抛异常
+            throw new RuntimeException("库名非法！！！！");
+        }
+
+        //接下来才可以调用admin的方法来创建库
+        //参数为 NamespaceDescriptor对象 但是不能通过new 的方式 因为他的构造器是私有的，所以需要看它是否有静态方法
+        //在NamespaceDescriptor中有create的静态方法 但他的返回值是build类， build类中有build方法可以构建出NamespaceDescriptor对象
+        admin.createNamespace(NamespaceDescriptor.create(nameSpace).build());
+    }
+
+    //还需要封装一个返回tablename对象，这个对象在多个地方都需要
+    // tablename对象通过 通过库名加表名来确定一个tablename对象
+    public static TableName gettbName(String ns, String tb){
+        //首先需要判断参数是否合法
+        if (StringUtils.isAnyBlank(ns,tb)) {
+            //说明参数不合法 可以直接抛异常
+            throw new RuntimeException("表名或者库名不合法");
+        }
+        //然后返回tablename对象  tablename有静态方法valueof 一个参数就必须库名:表名 两个参数的话就可以分开
+        TableName tableName = TableName.valueOf(ns, tb);
+        return tableName;
+    }
+
+    //接下来就是创建表的方法 ，在创建表之前还需要一个判断表是否存在的方法
+    public static Boolean isTableExist(Admin admin,String ns,String tbName) throws IOException {
+        // 因为tableExists方法参数是tableName ，而构建一个tablename 需要以库名 + 表名 ，所以参数需要库名
+        boolean b = admin.tableExists(TableName.valueOf(ns, tbName));
+        return b;
+
+    }
+
+    //接下来就是定义创建表的方法
+    //对于表的创建我们需要 提供namespace table名 列族 可以有多个列族 ，也即创建表的话，需要到列族，并且至少要有一个列族
+    public static void createTable(Admin admin,String ns,String tbName,String...columnFamiles) throws IOException {
+        //先检查参数是否合法
+        if (columnFamiles.length < 1 || StringUtils.isAnyBlank(ns,tbName)){
+            //说明参数不合法
+            throw new RuntimeException("参数不合法");
+        }
+        // 然后分别检查库和表是否已经存在
+        if (!checkNameSpaceExist(admin,ns)) {
+            //库不存在就需要创建库
+            createNameSpace(admin,ns);
+        }
+
+        if (!isTableExist(admin,ns,tbName)) {
+            // 表不存在就需要开始创建
+            // 在调用创建表的api的方法中 需要一个TableDescriptor 对象
+            //TableDescriptor对象是一个接口 并且构造器私有，需要通过TableDescriptorBuilder对象来构造
+            //TableDescriptorBuilder中有很多重要的方法，其中
+            // build方法可以返回一个TableDescriptor，那么在build返回之前肯定需要将有关表的参数进行封装的，
+            //作为TableDescriptor 即一个表描述对象，肯定需要通过库名加表名来确定一个表
+            //所以在TableDescriptorBuilder中有一个newbuilder方法 其返回还是TableDescriptorBuilder，方法参数是一个tablename，而正好
+            //一个tablename对象就对应一个表
+            //表确定好以后 需要确定列族 同样也是通过TableDescriptorBuilder中的setColumnFamilies或者setColumnFamily来确定
+            //方法的区别在于参数一个是ColumnFamilyDescriptor的集合或者单个ColumnFamilyDescriptor，
+            //ColumnFamilyDescriptor 对应列的描述符，如果说你有多个列的话可以直接封装成一个集合。然后直接set即可。
+            //ColumnFamilyDescriptor同样需要通过ColumnFamilyDescriptorBuilder类来得到
+            //也即在ColumnFamilyDescriptorBuilder中有build方法就可以得到一个ColumnFamilyDescriptor
+            //同样在build前需要给列族描述符来赋值参数
+            //在ColumnFamilyDescriptorBuilder中有两种方式来赋值列族参数分别是of方法以及newbuilder方法
+            //二者返回是不一样的 其中of方法直接可以得到一个ColumnFamilyDescriptor，而后者就返回的是ColumnFamilyDescriptorBuilder，然后再通过build方法就可以得到ColumnFamilyDescriptor
+            //另外参数也是有不同的，of方法中直接传递一个字节数组，即将stirng的列族直接通过工具类变成字节数组即可，
+            //而newbuilder方法可以直接传递一个string 或者传递一个字节数组也是可以的
+            //字节数组工具类是Bytes 实现常见类型和字节数组的转换，tobytes 将常见的数据类型转为字节数组 toT 将字节数组转为常见的数据类型
+
+            // 可以看到我们需要一个ColumnFamilyDescriptor集合 因为我们是可变数组，所以可以直接给一个集合
+            //思路就是将列族名封装成对应一个ColumnFamilyDescriptor 然后变成一个集合传递进去即可
+            //集合操作通过streamapi操作
+            List<ColumnFamilyDescriptor> columnFamilyDescriptors = Arrays.stream(columnFamiles).map(column -> ColumnFamilyDescriptorBuilder.of(column)).collect(Collectors.toList());
+
+            // 或者
+            // List<ColumnFamilyDescriptor> collect = Arrays.stream(columnFamiles).map(col -> ColumnFamilyDescriptorBuilder.of(Bytes.toBytes(col))).collect(Collectors.toList());
+            //或者
+            // List<ColumnFamilyDescriptor> collect = Arrays.stream(columnFamiles).map(col -> ColumnFamilyDescriptorBuilder.newBuilder(Bytes.toBytes(col)).build()).collect(Collectors.toList());
+
+            admin.createTable(TableDescriptorBuilder.newBuilder(gettbName(ns,tbName)).setColumnFamilies(columnFamilyDescriptors).build());
+        }
+
+
+    }
+
+
+}
--- realtime_dim/src/main/java/cn/cug/edu/realtime/dim/app/DimApp.java
+++ realtime_dim/src/main/java/cn/cug/edu/realtime/dim/app/DimApp.java
@@ -2,14 +2,26 @@ package cn.cug.edu.realtime.dim.app;
 
 import cn.cug.edu.common.base.BaseDataStreamApp;
 import cn.cug.edu.common.util.ConfigUtil;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonObjectFormatVisitor;
+import com.google.common.collect.Sets;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * author song
  * date 2024-08-03 21:37
  * Desc dimapp 用于继承baseapp 来实现job操作
  */
+@Slf4j
 public class DimApp extends BaseDataStreamApp {
 
     //每一个job必须要有一个main方法来实现job的操作
@@ -24,6 +36,69 @@ public class DimApp extends BaseDataStreamApp {
         //先简单的输出
         kfSource.print();
 
+        // 先做一个简单的etl
+        SingleOutputStreamOperator<String> etlData = etl(kfSource);
+
+
+
+
+    }
+
+    /**
+     *  在dim层之前实现对数据的etl
+     *   首先必须是json数据 在此前提下 生成的maxwell的数据格式中
+     *     1.库必须是实时数仓的数据库，因为maxwell可以监听多个数据库
+     *     2. 必须有表名
+     *     3. 必须有ts字段，
+     *     4. type类型 是根据maxwell来决定的，必须是insert update delete ，以及bootstrap-insert
+     *          因为maxwell监听的是对数据操作的日志，所以肯定是这个几种类型，delete类型也是需要的，如果是对表进行删除的话，假设
+     *          删除的是维度表，那么也需要后续操作的，另外在bootstrap-insert 也是一定的，因为在全量同步时，type的类型中只有bootstrap-insert
+     *          中data有数据，其他两个类型只是开始和结束的标识
+     *     5. 还有就是data中{} 必须要有数据
+     * @param kfSource
+     */
+    private SingleOutputStreamOperator<String> etl(DataStreamSource<String> kfSource) {
+        //通过fastjson工具的来解析json字符串，如果是json格式的字符串 就会解析成功成json对象，另外就是解析成json对象也方便对数据进行提取和
+        // 操作，比直接流中是json字符串好操作，当然这里只是etl 先不改变流中的数据格式
+
+        // 定义一个set集合 用来存放type的指定类型
+        HashSet<String> typeSet = Sets.newHashSet("insert", "update", "delete", "bootstrap-insert");
+        SingleOutputStreamOperator<String> filter = kfSource.filter(
+                jsonStr -> {
+                    try {
+                        JSONObject jsonObject = JSON.parseObject(jsonStr);
+                        // 在这里说明就是json格式的数据了
+                        //提取需要判断的数据
+                        String database = jsonObject.getString("database");
+                        String table = jsonObject.getString("table");
+                        String type = jsonObject.getString("type");
+                        String ts = jsonObject.getString("ts");
+                        String data = jsonObject.getString("data");
+                        // 库必须是cug_rt_gmall， table type ts 以及data不能为空 并且type必须是指定类型
+                        //data 的长度必须> 2 以上都是且的关系，只要有一个不满足就需要过滤掉
+                        // 另外type必须是指定类型 所以可以用一个set集合来定义好 类型
+                        return ConfigUtil.getString("BUSI_DATABASE").equals(database)
+                                &&
+                                // isnoneblank 表示的是可变参数中 只要有一个为null 空白字符 或者 ''
+                                // null 和 '' (表示的是空串的意思) '' 和 null的区别在于 null 表示啥也没有，长度为0 而'' 表示的是
+                                // 只有‘’ 里面的长度为0
+                                StringUtils.isNoneBlank(table, type, ts, data)
+                                &&
+                                typeSet.contains(type)
+                                &&
+                                data.length() > 2;
+
+                    } catch (Exception e) {
+                        //这里说明不是json格式数据 可以给一个提示，并且我们只是过滤 所以需要过滤掉不是json格式的数据
+                        //所以可以不抛异常 而是给出日志提示 并且返回false
+                        //在类中导入slf4j 注解 这个注解是lombook 给的 他会提供一个log对象，直接调用他的warn方法即可
+                        log.warn(jsonStr + "是非法的json格式");
+                        return false;
+                    }
+                }
+        );
+
+        return filter;
 
     }
 }
